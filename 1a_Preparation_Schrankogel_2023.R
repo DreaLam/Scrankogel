5f5a6fd2cda2a3dbbfda5aefb6e3aad30f4d99ed
unknown <andrea.lamprecht@boku.ac.at>
2024-01-15 14:01:46 +0100

    myconn adress added

--- 1a_Preparation_Schrankogel_2023.R
+++ 1a_Preparation_Schrankogel_2023.R
@@ -23,8 +23,9 @@ library(tidyverse)
 myconn <-odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:/Users/andreal/Documents/Projects/Schrankogel/Schrankogel_2023/Analysis/Schrankogel_4s/SCHRAN_1994_to_2023_MicroClim.accdb")
 #myconn <-odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=E:/Schrankogel/Schrankogel_2023/Analysis/Schrankogel_4s/SCHRAN_1994_to_2023_MicroClim.accdb")
 #myconn <-odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=X:/projects/SCHRANKOGEL/SCHRANKOGEL2023/Dateneingabe/SCHRAN_1994_to_2023_MicroClim.accdb")
+myconn <-odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:/Users/andreal/Documents/Projekte/Microclim/Schrankogel_GLORIA_analyse/SCHRAN_1994_to_2023_MicroClim.accdb")
 
-#or
+
 
 ## via ODBC-Datenquellen-Administrator (64bit)
 ## Benutzer DSN: choose 'MS Access Database', 'Hinzufuegen', choose 'Microsoft Access Driver', Namen for connection (here: "Schrankogel_2023"), Datenbank Auswaehlen, OK, OK
