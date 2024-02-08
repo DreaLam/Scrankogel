57aa1713376814623e45f444b6e4907acb04ff57
unknown <andrea.lamprecht@boku.ac.at>
2024-02-08 14:49:36 +0100

    start to play with data richness and cover

--- 4a_Richness_changes_per_pot.R
+++ 4a_Richness_changes_per_pot.R
@@ -1,2 +1,58 @@
 #### ANALYSE Richness change per plot ###
 
+## A) richness per plot and year
+rich <- spec %>%  group_by(fl_num,year)  %>% summarise( n_spec = length(year)) 
+##test
+#richT <- spec %>% mutate(nr = 1) %>% aggregate(cbind(nr)~ +fl_num + year, sum)
+#richT <- merge(rich, richT) %>% mutate(test = n_spec - nr)  ### always 0
+#rm(richT)
+
+rich <- merge(rich, Plotinfo[,c(1,3,4)], all.x = T)
+rich <- rich[,c(1,4,5,2,3)]
+
+write.table(rich , "../richness_per_plot_year.csv", sep = ";", row.names = F)
+
+
+## B) mean richness per TR and year: RAW -----------
+rich_mean_tr <-  ddply(rich, c("tr", "year"), summarise,
+                       N    = length(n_spec),
+                       mean = mean(n_spec),
+                       sd   = sd(n_spec),
+                       se   = sd / sqrt(N)
+)
+
+rich_mean_tr[,4:6] <- round(rich_mean_tr[,4:6], 2)
+colnames(rich_mean_tr)[6] <- 'SE'
+write.table(rich_mean_tr , "../rich_mean_tr_RAW.csv", sep = ";", row.names = F)
+
+plot(rich$year, rich$n_spec)
+plot(rich_mean_tr$year, rich_mean_tr$mean)
+
+ggplot(rich_mean_tr,  aes(x=year, y=mean,  colour= tr)) +
+  geom_point(size=2 )+
+  geom_line(lty = 2) +
+  scale_colour_brewer(palette="Set2") +
+  geom_errorbar(aes(ymin=mean-SE, ymax=mean+SE), width=.2) +
+  ylab ("Species richness per plot (mean +/- SE)") +
+  ggtitle("plots, raw") +
+  theme_bw() +
+  ggsave("./../plots_graphs/richness_mean_tr_years_raw.tiff")      ####### Be careful with 2004: number of plots is much smaller.
+
+rm(rich_mean_tr)
+
+### C) Analyses
+histogram( ~ n_spec | year, type = "count" , data=rich)
+glm_gaus<-glm(rich$n_spec~1)
+glm_pois<-glm(rich$n_spec~1, family = "poisson")
+glm_NB<-glm.nb(rich$n_spec~1)
+# Diagnostics on the NULL models
+par(mfrow=c(2,4))
+plot(glm_gaus, main="gaussian")  
+plot(glm_pois, main="poisson")
+anova(glm_gaus,glm_pois, glm_NB)  
+
+AIC(glm_gaus)
+AIC(glm_pois)
+AIC(glm_NB)
+
+rm(glm_gaus,glm_nb,glm_NB, glm_pois)
--- 5a_Cover_changes_per_pot.R
+++ 5a_Cover_changes_per_pot.R
@@ -1,11 +1,14 @@
 #### ANALYSE COVER CHANGE PER PLOT
 
-library(Matrix)
-library(lme4)
+#library(Matrix)
+#library(lme4)
+library(plyr)
+library(tidyverse)
+
 
 str(spec)
 
-## Species per plot
+## A) Species cover per plot and year
 specCov <- merge(spec, Plotinfo[,c(1,3,4)], all.x = T)
 specCov$yearN <- as.numeric(specCov$year)
 str(specCov)
@@ -21,15 +24,61 @@ specCov <- specCov %>% replace(is.na(.), 0)
 
 
 ggplot(specCov, aes(x=covSum)) + geom_histogram(binwidth=.5)  ### right-skewed distribution
+### using glmer with family Gamma?
+
+### plot raw data
+ggplot(specCov,  aes(x=year, y=covSum,  colour= tr)) +
+  geom_point(size=2 )+
+  geom_line(lty = 2) +
+  scale_colour_brewer(palette="Set2") +
+  #geom_errorbar(aes(ymin=mean-SE, ymax=mean+SE), width=.2) +
+  ylab ("Species cover per plot (mean +/- SE)") +
+  ggtitle("plots, raw") +
+  theme_bw()# +
+  #ggsave("./../plots/Cov_SUC_years_raw.tiff")
 
 
+## B) mean cover per tr and year: RAW -----------
+Cover_mean_tr <- ddply(specCov, c("tr", "year"), summarise,
+                          N    = length(covSum),
+                          mean = mean(covSum),
+                          sd   = sd(covSum),
+                          se   = sd / sqrt(N)
+)
 
+Cover_mean_tr[,4:6] <- round(Cover_mean_SN_TR[,4:6], 2)
+colnames(Cover_mean_tr)[6] <- 'SE'
+write.table(Cover_mean_tr , "../Cover_mean_tr_RAW.csv", sep = ";", row.names = F)
+
+plot(specCov$year, specCov$covSum)
+plot(Cover_mean_tr$year, Cover_mean_tr$mean)
+
+ggplot(Cover_mean_tr,  aes(x=year, y=mean,  colour= tr)) +
+  geom_point(size=2 )+
+  geom_line(lty = 2) +
+  scale_colour_brewer(palette="Set2") +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.2) +
+  ylab ("Species cover per plot (mean +/- SE)") +
+  ggtitle("plots, raw") +
+  theme_bw() +
+ggsave("./../plots_graphs/Cov_mean_tr_years_raw.tiff")      ####### it seems, that mean cover in tr1 increased in 2023, others decreased. Be careful with 2004: number of plots is much smaller.
+
+rm(Cover_mean_tr)
 #########
-#########
+### C) Analyses
 
+histogram( ~ covSum | year,  data=specCov)   ### absolutely right skewed
 
+glm_gaus<-glm(specCov$covSum~1)
+glm_pois<-glm(specCov$covSum~1, family = "poisson")  ## lot of warnings.
+glm_nb <- glm.nb(specCov$covSum~1) ## lot of warnings.
+glm_gamma <- glm(specCov$covSum~1, family = "Gamma") ## non-positive values not allowed for the 'Gamma' family
+# Diagnostics on the NULL models
+par(mfrow=c(3,4))
+plot(glm_gaus, main="gaussian")  
+plot(glm_pois, main="poisson")
+plot(glm_nb, main="neg.bin")
+AIC(glm_gaus, glm_pois, glm_nb)  ### lowest nb
 
-CovChange_all4 <- lmer(covSum ~ year  + (1|block/tr/fl_num),  data = specCov)
+CovChange_all4 <- glm.nb(covSum ~ year  + (1|block/tr/fl_num),  data = specCov )   ### lots of warnings
 summary(CovChange_all4)
-
-ggplot(specCov, aes(x = yearN, y = cover)) + geom_point()
